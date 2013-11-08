ec17315a5fc3872e163ea6392f78f796723d5830
Paul Shi <hipaulshi@gmail.com>
2013-11-08 12:48:21 -0800

    Fixed issue with pulling too often from weather underground API.
    Now only pulls when date changes at midnight, so sunrise and sunset is
    updated at midnight. Also the program is now in classes, easier to
    manage.
    
    Fixed #1

--- colorChange.py
+++ colorChange.py
@@ -2,68 +2,104 @@ import requests
 import datetime
 from config import *
 
-def getColorTemperature(mode):
-	# max range of philip hue color temperature setting
-	maxRangeVal = 153.0
-	minRangeVal = 500.0
-	# which corresponds to
-	maxRange = 6500.0 #K
-	minRange = 2000.0 #K
-	# My Setting 
-	morningRange = 6500.0 #K
-	nightRange = 3400.0 #K
+class LastUpdate:
+	def __init__(self):
+		self.date = None
+		self.sunrise_h = None
+		self.sunrise_m = None
+		self.sunset_h = None
+		self.sunset_m = None
 
-	perKValueChnage = (abs(maxRangeVal -  minRangeVal) / abs(maxRange - minRange))
-	myMorningVal = maxRangeVal + perKValueChnage*(maxRange - morningRange)
-	myNightVal =  minRangeVal + perKValueChnage*(minRange - nightRange)
+	def updateLastUpdate(self, date,sunrise_h,sunrise_m,sunset_h,sunset_m):
+		self.date = date
+		self.sunrise_h = sunrise_h
+		self.sunrise_m = sunrise_m
+		self.sunset_h = sunset_h
+		self.sunset_m = sunset_m
 
-	# print int(myMorningVal)
-	# print int(myNightVal)
+	def getLastUpdate(self):
+		return (self.sunrise_h, self.sunrise_m, self.sunset_h, self.sunset_m)
 
-	if mode == "hi":
-		return int(myMorningVal)
-	elif mode == "low":
-		return int(myNightVal)
-	else:
-		return -1
+	def getLastUpdateDate(self):
+		return self.date
 
-def getSunInfo():
-	url = "http://api.wunderground.com/api/"+weatherUnderGroundAPIKey+"/astronomy/q/"+zipCode+".json"
-	# print url
-	r = requests.get(url)
-	# print r.status_code
-	# print r.text
-	rjson = r.json()
-	sunrise_h = rjson['sun_phase']['sunrise']['hour']
-	sunrise_m = rjson['sun_phase']['sunrise']['minute']
-	sunset_h = rjson['sun_phase']['sunset']['hour']
-	sunset_m = rjson['sun_phase']['sunset']['minute']
-	currenttime = datetime.datetime.now();
+weatherUndergroundUrl = "http://api.wunderground.com/api/"+weatherUnderGroundAPIKey+"/astronomy/q/"+zipCode+".json"
 
-	#this makes sure that the time frame is always constructed form today's date
-	reconstructed_sunrise_time = datetime.datetime(currenttime.year, currenttime.month, currenttime.day, int(sunrise_h), int(sunrise_m))
-	reconstructed_sunset_time = datetime.datetime(currenttime.year, currenttime.month, currenttime.day, int(sunset_h), int(sunset_m))
-	print "sunrise time: {}".format(reconstructed_sunrise_time)
-	print "sunset time: {}".format(reconstructed_sunset_time)
-	print "current time: {}".format(currenttime)
+class ColorTemperatureChange:
+	def __init__(self):
+		self.lastUpdate = LastUpdate()
 
-	flagA = currenttime > reconstructed_sunrise_time
-	flagB =  currenttime > reconstructed_sunset_time
+	def getColorTemperature(self,mode):
+		# max range of philip hue color temperature setting
+		maxRangeVal = 153.0
+		minRangeVal = 500.0
+		# which corresponds to
+		maxRange = 6500.0 #K
+		minRange = 2000.0 #K
+		# My Setting 
+		morningRange = 6500.0 #K
+		nightRange = 3400.0 #K
 
-	if flagA and not flagB:
-		return 1
-	elif flagA and flagB:
-		return 0
-	elif not flagA and not flagB:
-		return 0
-	else:
-		return -1
-	
-def setMode():
-	SunInfo = getSunInfo()
-	if SunInfo==1:
-		return "hi"
-	elif SunInfo == 0:
-		return "low"
-	else:
-		return "time logic error!"
+		perKValueChnage = (abs(maxRangeVal -  minRangeVal) / abs(maxRange - minRange))
+		myMorningVal = maxRangeVal + perKValueChnage*(maxRange - morningRange)
+		myNightVal =  minRangeVal + perKValueChnage*(minRange - nightRange)
+
+		# print int(myMorningVal)
+		# print int(myNightVal)
+
+		if mode == "hi":
+			return int(myMorningVal)
+		elif mode == "low":
+			return int(myNightVal)
+		else:
+			return -1
+
+	def determineHueStates(self):
+		today = datetime.date.today()
+		if self.lastUpdate.getLastUpdateDate() != today:
+			#get sunrise and sunset from weather underground
+
+			print ("getting new sunrise and sunset info from weather Underground for Today")
+			r = requests.get(weatherUndergroundUrl)
+			rjson = r.json()
+			self.lastUpdate.updateLastUpdate( \
+				today, \
+				rjson['sun_phase']['sunrise']['hour'], \
+				rjson['sun_phase']['sunrise']['minute'], \
+				rjson['sun_phase']['sunset']['hour'], \
+				rjson['sun_phase']['sunset']['minute'] \
+			)
+			
+
+		currenttime = datetime.datetime.now();
+
+		(sunrise_h, sunrise_m, sunset_h, sunset_m) = self.lastUpdate.getLastUpdate();
+
+		#this makes sure that the time frame is always constructed form today's date
+		reconstructed_sunrise_time = datetime.datetime(currenttime.year, currenttime.month, currenttime.day, int(sunrise_h), int(sunrise_m))
+		reconstructed_sunset_time = datetime.datetime(currenttime.year, currenttime.month, currenttime.day, int(sunset_h), int(sunset_m))
+		
+		print "sunrise time: {}".format(reconstructed_sunrise_time)
+		print "sunset time: {}".format(reconstructed_sunset_time)
+		print "current time: {}".format(currenttime)
+
+		flagA = currenttime > reconstructed_sunrise_time
+		flagB =  currenttime > reconstructed_sunset_time
+
+		if flagA and not flagB:
+			return 1
+		elif flagA and flagB:
+			return 0
+		elif not flagA and not flagB:
+			return 0
+		else:
+			return -1
+
+	def setHueMode(self):
+		SunInfo = self.determineHueStates()
+		if SunInfo==1:
+			return "hi"
+		elif SunInfo == 0:
+			return "low"
+		else:
+			return "time logic error!"
--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from config import myUrl
 import json
 import requests
 
+myColorChange = ColorTemperatureChange();
+
 while (1):
-	
-	whichMode = setMode()
-	ct = getColorTemperature(whichMode)
+
+	whichMode = myColorChange.setHueMode()
+	ct = myColorChange.getColorTemperature(whichMode)
 
 	payload = {'colormode': 'ct', 'ct': ct}
 	response=requests.post(myUrl,data=json.dumps(payload))
